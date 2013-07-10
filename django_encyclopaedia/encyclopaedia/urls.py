ddf7820b308009709dafad43cf67ba7de0072f82
Катя Неповинных <kwadrat@linux-8v9j.site>
2013-07-10 09:54:31 +0600

    Добавлены страницы index и section. Добавлены шаблоны.

--- django_encyclopaedia/encyclopaedia/urls.py
+++ django_encyclopaedia/encyclopaedia/urls.py
@@ -5,4 +5,6 @@ from encyclopaedia import views
 
 urlpatterns = patterns('',
     url(r'^(?P<article_id>\d+)/$', views.article, name='article'),
+    url(r'^$', views.index, name='index'),
+    url(r'^section/(?P<letter>\w{1})/$', views.section, name='section'),
 )
--- django_encyclopaedia/encyclopaedia/views.py
+++ django_encyclopaedia/encyclopaedia/views.py
@@ -1,6 +1,27 @@
 from django.http import HttpResponse
 from encyclopaedia.models import Article
+from django.template import Context, loader
 
 def article(request, article_id):
 	article = Article.objects.get(pk=article_id)
 	return HttpResponse(article.text)
+
+def index(request):
+	letter_list = Article.objects.all()
+	x=[a.name[0] for a in letter_list]
+	letter_list=sorted(list(set(x)))
+
+	template = loader.get_template('encyclopaedia/index.html')
+	context = Context({
+        'sections': letter_list,
+    })
+	return HttpResponse(template.render(context))
+
+def section(request, letter):
+	q1 = Article.objects.filter(name__startswith=letter)
+
+	template = loader.get_template('encyclopaedia/section.html')
+	context = Context({
+        'articles': [a for a in q1],
+    })
+	return HttpResponse(template.render(context))
